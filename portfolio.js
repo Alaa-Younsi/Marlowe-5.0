4ef1fb6219cff8b61dbc96f83121acf036d5245a
Alaa Younsi <alaayounsi777@gmail.com>
2023-05-14 01:47:10 +0100

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,814 @@
+$(document).ready(function(){
+
+    $("#Skills-button").click(function(){
+        $("#Home").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Home-button").click(function(){
+        $("#Home").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Skills-button").click(function(){
+        $("#Skills").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Home-button").click(function(){
+        $("#Skills").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Info-button").click(function(){
+        $("#Home").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Home-button").click(function(){
+        $("#Info").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Info-button").click(function(){
+        $("#Info").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Skills-button").click(function(){
+        $("#Info").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Info-button").click(function(){
+        $("#Skills").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Certificates-button").click(function(){
+        $("#Home").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Certificates-button").click(function(){
+        $("#Skills").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Certificates-button").click(function(){
+        $("#Info").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Certificates-button").click(function(){
+        $("#Certificates").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Home-button").click(function(){
+        $("#Certificates").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Info-button").click(function(){
+        $("#Certificates").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Skills-button").click(function(){
+        $("#Certificates").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Works-button").click(function(){
+        $("#Home").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Works-button").click(function(){
+        $("#Skills").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Works-button").click(function(){
+        $("#Info").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Works-button").click(function(){
+        $("#Certificates").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Home-button").click(function(){
+        $("#Works").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Skills-button").click(function(){
+        $("#Works").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Info-button").click(function(){
+        $("#Works").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Certificates-button").click(function(){
+        $("#Works").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Works-button").click(function(){
+        $("#Works").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Blog-button").click(function(){
+        $("#Blog").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Blog-button").click(function(){
+        $("#Home").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Blog-button").click(function(){
+        $("#Skills").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Blog-button").click(function(){
+        $("#Info").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Blog-button").click(function(){
+        $("#Works").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Blog-button").click(function(){
+        $("#Certificates").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Home-button").click(function(){
+        $("#Blog").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Works-button").click(function(){
+        $("#Blog").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Skills-button").click(function(){
+        $("#Blog").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Info-button").click(function(){
+        $("#Blog").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Certificates-button").click(function(){
+        $("#Blog").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Contact-button").click(function(){
+        $("#Contact").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Contact-button").click(function(){
+        $("#Home").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Contact-button").click(function(){
+        $("#Skills").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Contact-button").click(function(){
+        $("#Info").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Contact-button").click(function(){
+        $("#Works").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Contact-button").click(function(){
+        $("#Certificates").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Contact-button").click(function(){
+        $("#Blog").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Home-button").click(function(){
+        $("#Contact").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Skills-button").click(function(){
+        $("#Contact").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Works-button").click(function(){
+        $("#Contact").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Info-button").click(function(){
+        $("#Contact").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Certificates-button").click(function(){
+        $("#Contact").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Blog-button").click(function(){
+        $("#Contact").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Off-button").click(function(){
+        $("#Tv").slideUp(1000);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Off-button").click(function(){
+        $("#Main-section").slideUp(1000);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Off-button").click(function(){
+        $("#On-button").fadeIn(2000);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#On-button").click(function(){
+        $("#Tv").slideDown(1000);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#On-button").click(function(){
+        $("#On-button").fadeOut(250);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Home-button").click(function(){
+        $("#Main-section").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Skills-button").click(function(){
+        $("#Skills-section").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Skills-button").click(function(){
+        $("#Hat-image").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Home-button").click(function(){
+        $("#Skills-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Info-button").click(function(){
+        $("#Skills-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Works-button").click(function(){
+        $("#Skills-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Certificates-button").click(function(){
+        $("#Skills-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Blog-button").click(function(){
+        $("#Skills-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Contact-button").click(function(){
+        $("#Skills-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Home-button").click(function(){
+        $("#Hat-image").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Skills-button").click(function(){
+        $("#Main-section").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Info-button").click(function(){
+        $("#Info-section").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Info-button").click(function(){
+        $("#Hat-image").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Home-button").click(function(){
+        $("#Info-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Skills-button").click(function(){
+        $("#Info-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Works-button").click(function(){
+        $("#Info-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Certificates-button").click(function(){
+        $("#Info-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Blog-button").click(function(){
+        $("#Info-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Contact-button").click(function(){
+        $("#Info-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Info-button").click(function(){
+        $("#Main-section").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Works-button").click(function(){
+        $("#Works-section").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Works-button").click(function(){
+        $("#Hat-image").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Home-button").click(function(){
+        $("#Works-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Skills-button").click(function(){
+        $("#Works-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Info-button").click(function(){
+        $("#Works-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Certificates-button").click(function(){
+        $("#Works-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Blog-button").click(function(){
+        $("#Works-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Contact-button").click(function(){
+        $("#Works-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Works-button").click(function(){
+        $("#Main-section").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Certificates-button").click(function(){
+        $("#Certificates-section").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Certificates-button").click(function(){
+        $("#Hat-image").fadeOut(1);
+      });
+  });
+  
+  $(document).ready(function(){
+
+    $("#Home-button").click(function(){
+        $("#Certificates-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Skills-button").click(function(){
+        $("#Certificates-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Info-button").click(function(){
+        $("#Certificates-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Works-button").click(function(){
+        $("#Certificates-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Blog-button").click(function(){
+        $("#Certificates-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Contact-button").click(function(){
+        $("#Certificates-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Certificates-button").click(function(){
+        $("#Main-section").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Blog-button").click(function(){
+        $("#Blog-section").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Blog-button").click(function(){
+        $("#Hat-image").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Home-button").click(function(){
+        $("#Blog-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Skills-button").click(function(){
+        $("#Blog-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Info-button").click(function(){
+        $("#Blog-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Works-button").click(function(){
+        $("#Blog-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Certificates-button").click(function(){
+        $("#Blog-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Contact-button").click(function(){
+        $("#Blog-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Blog-button").click(function(){
+        $("#Main-section").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Contact-button").click(function(){
+        $("#Contact-section").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Contact-button").click(function(){
+        $("#Hat-image").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Home-button").click(function(){
+        $("#Contact-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Skills-button").click(function(){
+        $("#Contact-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Info-button").click(function(){
+        $("#Contact-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Works-button").click(function(){
+        $("#Contact-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Certificates-button").click(function(){
+        $("#Contact-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Blog-button").click(function(){
+        $("#Contact-section").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Contact-button").click(function(){
+        $("#Main-section").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+        $("#Marlowe").fadeOut(4000);
+      
+  });
+
+  $(document).ready(function(){
+
+    $("#Marlowee").fadeOut(1250);
+  
+});
+
+  $(document).ready(function(){
+
+    $("#Portfolio").fadeOut(1250);
+
+});
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
